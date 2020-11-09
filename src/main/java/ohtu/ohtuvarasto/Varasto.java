414e9e3d3764245eaa89b8b5ac1ff9143fb32f4f
Benjamin Blinnikka <benjamin.blinnikka@hotmail.com>
2020-11-09 17:01:54 +0200

    Actions: Breaking checkstyle

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -22,7 +22,8 @@ public class Varasto {
     }
 
     private void varastonTilavuus(double tilavuus) {
-        if (tilavuus > 0.0) {
+        if (tilavuus > 0.0) 
+        {
             this.tilavuus = tilavuus;
         } else {
             this.tilavuus = 0.0;
